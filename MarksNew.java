6bc1d99a505e3bc81697b4cbd28ea776e815d0c2
IT25104066 <it25104066@my.sliit.lk>
2026-07-25 11:02:37 +0530

    Made changes to Q3

--- MarksNew.java
+++ MarksNew.java
@@ -7,9 +7,9 @@ public class MarksNew{
         int n = Integer.parseInt(sc.nextLine());
         int marks[][] = new int[n][3];
 
-        System.out.print("Enter:\n1:Add marks\n2:Get student average\n3:Get subject average\n4:Get total mark of student\ngrades:Display grade\n6:Exit\n");
+        System.out.print("Enter:\n1:Add marks\n2:Get student average\n3:Get subject average\n4:Get total mark of student\ngrades:Display grade\n5:Update marks\n6:Exit\n");
         String choice = sc.nextLine();
-        while (!choice.equals("1") && !choice.equals("2") && !choice.equals("3") && !choice.equals("4") && !choice.equals("grades") && !choice.equals("6")) {
+        while (!choice.equals("1") && !choice.equals("2") && !choice.equals("3") && !choice.equals("4") && !choice.equals("grades") && !choice.equals("5") && !choice.equals("6")) {
             System.out.print("Invalid choice, enter again: ");
             choice = sc.nextLine();
         }
@@ -92,14 +92,31 @@ public class MarksNew{
                     }
                     System.out.println();
                 }
+            } else if (choice.equals("5")) {
+                System.out.print("Enter student id to update the marks: ");
+                int stdId = Integer.parseInt(sc.nextLine());
+                while (stdId<1 || stdId>n) {
+                    System.out.print("Invalid choice, enter again: ");
+                    stdId = Integer.parseInt(sc.nextLine());
+                }
+                System.out.println("Enter the subject id to update the marks:\n1:Mathematics\n2:Chemistry\n3:Physics");
+                int subID = Integer.parseInt(sc.nextLine());
+                while (subID<1 || subID>3) {
+                    System.out.print("Invalid choice, enter again: ");
+                    subID = Integer.parseInt(sc.nextLine());
+                }
+                System.out.printf("Enter the new mark for student %d in subject id  %d: ", stdId, subID);
+                int inputMark = Integer.parseInt(sc.nextLine());
+                marks[stdId-1][subID-1] = inputMark;
+                System.out.println("Mark updated successfully.");
             } else if (choice.equals("6")) {
                 System.out.println("Exiting...");
                 break;
             }
 
-            System.out.print("Enter:\n1:Add marks\n2:Get student average\n3:Get subject average\n4:Get total mark of student\ngrades:Display grade\n6:Exit\n");
+            System.out.print("Enter:\n1:Add marks\n2:Get student average\n3:Get subject average\n4:Get total mark of student\ngrades: Display grades\n5:Update marks\n6:Exit\n");
             choice = sc.nextLine();
-            while (!choice.equals("1") && !choice.equals("2") && !choice.equals("3") && !choice.equals("4") && !choice.equals("grades") && !choice.equals("6")) {
+            while (!choice.equals("1") && !choice.equals("2") && !choice.equals("3") && !choice.equals("4") && !choice.equals("grades") && !choice.equals("5") && !choice.equals("6")) {
                 System.out.print("Invalid choice, enter again: ");
                 choice = sc.nextLine();
             }
